789ef86538290371c93b3f0535b97ba387344673
anuraghazra <hazru.anurag@gmail.com>
2019-02-20 18:46:45 +0530

    updated express static folder / heroku giving not found json

--- server/app.js
+++ server/app.js
@@ -7,18 +7,18 @@ const app = express();
 const path = require('path');
 
 // ADD THIS LINE
-app.use(express.static('client/build'));
-// If no API routes are hit, send the React app
-const router = express.Router();
-router.use(function(req, res) {
-	res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.use(express.static('client/build'));
+// // If no API routes are hit, send the React app
+// const router = express.Router();
+// router.use(function(req, res) {
+// 	res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 // Serve static files from the React app
-// app.use(express.static(path.join(__dirname, '../client/build')));
-// app.get('/', (req, res) => {
-//   res.sendFile(path.join(__dirname + '../client/build/index.html'));
-// });
+app.use(express.static(path.join(__dirname, '../client/build')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '../client/build/index.html'));
+});
 
 // middlewares
 app.use(cors);
